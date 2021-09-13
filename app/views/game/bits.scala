lichess-org/lila

Link Correspondence FAQ in games and cleanup variantLink

app/views/game/bits.scala
@@ -1,9 +1,12 @@
 package views.html.game
 
+import play.api.i18n.Lang
+
 import lila.api.Context
 import lila.app.templating.Environment._
 import lila.app.ui.ScalatagsTemplate._
 import lila.game.{ Game, Pov }
+import lila.rating.PerfType.Correspondence
 
 import controllers.routes
 
@@ -37,18 +40,42 @@ object bits {
 
   def variantLink(
       variant: chess.variant.Variant,
-      name: String,
-      initialFen: Option[chess.format.FEN] = None
-  ) =
-    a(
+      perfType: Option[lila.rating.PerfType] = None,
+      initialFen: Option[chess.format.FEN] = None,
+      shortName: Boolean = false
+  )(implicit lang: Lang): Frag = {
+    def link(
+        href: String,
+        title: String,
+        name: String
+    ) = a(
       cls := "variant-link",
-      href := (variant match {
-        case chess.variant.Standard => "https://en.wikipedia.org/wiki/Chess"
-        case chess.variant.FromPosition =>
-          s"""${routes.Editor.index}?fen=${initialFen.??(_.value.replace(' ', '_'))}"""
-        case v => routes.Page.variant(v.key).url
-      }),
+      st.href := href,
       targetBlank,
-      title := variant.title
+      st.title := title
     )(name)
+
+    if (variant.exotic)
+      link(
+        href = (variant match {
+          case chess.variant.FromPosition =>
+            s"""${routes.Editor.index}?fen=${initialFen.??(_.value.replace(' ', '_'))}"""
+          case v => routes.Page.variant(v.key).url
+        }),
+        title = variant.title,
+        name = (if (shortName && variant == chess.variant.KingOfTheHill) variant.shortName
+                else variant.name).toUpperCase
+      )
+    else
+      perfType match {
+        case Some(Correspondence) =>
+          link(
+            href = s"${routes.Main.faq}#correspondence",
+            title = Correspondence.desc,
+            name = Correspondence.trans
+          )
+        case Some(pt) => span(title := pt.desc)(pt.trans)
+        case _        => variant.name.toUpperCase
+      }
+  }
 }

WandererXII/lishogi

app/views/game/bits.scala
@@ -1,10 +1,13 @@
 package views.html.game
 
+import play.api.i18n.Lang
+
 import lila.api.Context
 import lila.app.templating.Environment._
 import lila.app.ui.ScalatagsTemplate._
 import lila.game.{ Game, Player, Pov }
 import lila.user.Title
+import lila.rating.PerfType.Correspondence
 
 import controllers.routes
 
@@ -67,21 +70,45 @@ object bits {
 
   def variantLink(
       variant: shogi.variant.Variant,
-      name: String,
-      initialFen: Option[shogi.format.FEN] = None
-  ) =
-    a(
+      perfType: Option[lila.rating.PerfType] = None,
+      initialFen: Option[shogi.format.FEN] = None,
+      //shortName: Boolean = false
+  )(implicit lang: Lang): Frag = {
+    def link(
+        href: String,
+        title: String,
+        name: String
+    ) = a(
       cls := "variant-link",
-      href := (variant match {
-        case shogi.variant.Standard => "https://en.wikipedia.org/wiki/Shogi"
-        case shogi.variant.FromPosition =>
-          s"""${routes.Editor.index()}?fen=${initialFen.??(_.value.replace(' ', '_'))}"""
-        case v => routes.Page.variant(v.key).url
-      }),
+      st.href := href,
       rel := "nofollow",
       target := "_blank",
-      title := variant.title
+      st.title := title
     )(name)
+
+    if (variant.exotic)
+      link(
+        href = (variant match {
+          case shogi.variant.FromPosition =>
+            s"""${routes.Editor.index}?fen=${initialFen.??(_.value.replace(' ', '_'))}"""
+          case v => routes.Page.variant(v.key).url
+        }),
+        //title = variant.title,
+        //name = (if (shortName && variant == shogi.variant.MiniShogi) variant.shortName
+                //else variant.name).toUpperCase
+      )
+    else
+      perfType match {
+        case Some(Correspondence) =>
+          link(
+            href = s"${routes.Main.faq}#correspondence",
+            title = Correspondence.desc,
+            name = Correspondence.trans
+          )
+        case Some(pt) => span(title := pt.desc)(pt.trans)
+        case _        => variant.name.toUpperCase
+      }
+  }
 
   private def playerTitle(player: Player) =
     player.userId.flatMap(lightUser).flatMap(_.title) map Title.apply map { t =>