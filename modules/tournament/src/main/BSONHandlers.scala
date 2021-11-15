lichess-org/lila

tournaments from position WIP

modules/tournament/src/main/BSONHandlers.scala
@@ -1,13 +1,15 @@
 package lila.tournament
 
 import chess.Clock.{ Config => ClockConfig }
+import chess.format.FEN
 import chess.variant.Variant
 import chess.{ Mode, StartingPosition }
+import reactivemongo.api.bson._
+
 import lila.db.BSON
 import lila.db.dsl._
 import lila.rating.PerfType
 import lila.user.User.lichessId
-import reactivemongo.api.bson._
 
 object BSONHandlers {
 
@@ -62,9 +64,11 @@ object BSONHandlers {
   implicit val tournamentHandler = new BSON[Tournament] {
     def reads(r: BSON.Reader) = {
       val variant = r.intO("variant").fold[Variant](Variant.default)(Variant.orDefault)
-      val position: StartingPosition = r.strO("fen").flatMap(Thematic.byFen) orElse
-        r.strO("eco").flatMap(Thematic.byEco) getOrElse // for BC
-        StartingPosition.initial
+      val position: Either[StartingPosition, FEN] =
+        r.strO("fen") map { f =>
+          Thematic.byFen(f).fold[Either[StartingPosition, FEN]](Right(FEN(f)))(Left(_))
+        } getOrElse
+          Left(r.strO("eco").flatMap(Thematic.byEco) | StartingPosition.initial)
       val startsAt   = r date "startsAt"
       val conditions = r.getO[Condition.All]("conditions") getOrElse Condition.All.empty
       Tournament(
@@ -85,7 +89,8 @@ object BSONHandlers {
           doc   <- r.getO[Bdoc]("schedule")
           freq  <- doc.getAsOpt[Schedule.Freq]("freq")
           speed <- doc.getAsOpt[Schedule.Speed]("speed")
-        } yield Schedule(freq, speed, variant, position, startsAt, conditions),
+          pos   <- position.left.toOption
+        } yield Schedule(freq, speed, variant, pos, startsAt, conditions),
         nbPlayers = r int "nbPlayers",
         createdAt = r date "createdAt",
         createdBy = r strO "createdBy" getOrElse lichessId,
@@ -99,13 +104,17 @@ object BSONHandlers {
     }
     def writes(w: BSON.Writer, o: Tournament) =
       $doc(
-        "_id"         -> o.id,
-        "name"        -> o.name,
-        "status"      -> o.status,
-        "clock"       -> o.clock,
-        "minutes"     -> o.minutes,
-        "variant"     -> o.variant.some.filterNot(_.standard).map(_.id),
-        "fen"         -> o.position.some.filterNot(_.initial).map(_.fen),
+        "_id"     -> o.id,
+        "name"    -> o.name,
+        "status"  -> o.status,
+        "clock"   -> o.clock,
+        "minutes" -> o.minutes,
+        "variant" -> o.variant.some.filterNot(_.standard).map(_.id),
+        "fen" -> (o.position match {
+          case Right(f)              => f.value.some
+          case Left(p) if !p.initial => p.fen.some
+          case _                     => none
+        }),
         "mode"        -> o.mode.some.filterNot(_.rated).map(_.id),
         "password"    -> o.password,
         "conditions"  -> o.conditions.ifNonEmpty,

modules/tournament/src/main/JsonView.scala
@@ -1,5 +1,7 @@
 package lila.tournament
 
+import chess.format.FEN
+import chess.StartingPosition
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 import play.api.i18n.Lang
@@ -11,7 +13,6 @@ import lila.common.{ GreatPlayer, LightUser, Uptime }
 import lila.game.{ Game, LightPov }
 import lila.hub.LightTeam.TeamID
 import lila.memo.CacheApi._
-import lila.quote.Quote.quoteWriter
 import lila.rating.PerfType
 import lila.socket.Socket.SocketVersion
 import lila.user.{ LightUserApi, User }
@@ -116,7 +117,7 @@ final class JsonView(
           )
           .add("spotlight" -> tour.spotlight)
           .add("berserkable" -> tour.berserkable)
-          .add("position" -> full.option(tour.position).filterNot(_.initial).map(positionJson))
+          .add("position" -> full.??(positionJson(tour.position)))
           .add("verdicts" -> verdicts.map(Condition.JSONHandlers.verdictsFor(_, lang)))
           .add("schedule" -> tour.schedule.map(scheduleJson))
           .add("private" -> tour.isPrivate)
@@ -521,13 +522,25 @@ object JsonView {
     )
   }
 
-  private[tournament] def positionJson(s: chess.StartingPosition) =
-    Json.obj(
-      "eco"      -> s.eco,
-      "name"     -> s.name,
-      "wikiPath" -> s.wikiPath,
-      "fen"      -> s.fen
-    )
+  private[tournament] def positionJson(s: Either[StartingPosition, FEN]): Option[JsObject] = s match {
+    case Left(pos) if pos.initial => none
+    case Left(pos) =>
+      Json
+        .obj(
+          "eco"      -> pos.eco,
+          "name"     -> pos.name,
+          "wikiPath" -> pos.wikiPath,
+          "fen"      -> pos.fen
+        )
+        .some
+    case Right(fen) =>
+      Json
+        .obj(
+          "name" -> "Custom position",
+          "fen"  -> fen.value
+        )
+        .some
+  }
 
   implicit private[tournament] val spotlightWrites: OWrites[Spotlight] = OWrites { s =>
     Json

modules/tournament/src/main/Tournament.scala
@@ -1,6 +1,7 @@
 package lila.tournament
 
 import chess.Clock.{ Config => ClockConfig }
+import chess.format.FEN
 import chess.{ Mode, Speed, StartingPosition }
 import org.joda.time.{ DateTime, Duration, Interval }
 import play.api.i18n.Lang
@@ -19,7 +20,7 @@ case class Tournament(
     clock: ClockConfig,
     minutes: Int,
     variant: chess.variant.Variant,
-    position: StartingPosition,
+    position: Either[StartingPosition, FEN],
     mode: Mode,
     password: Option[String] = None,
     conditions: Condition.All,
@@ -136,6 +137,8 @@ case class Tournament(
 
   def ratingVariant = if (variant.fromPosition) chess.variant.Standard else variant
 
+  def initialPosition = position.left.exists(_.initial)
+
   lazy val looksLikePrize = !isScheduled && lila.common.String.looksLikePrize(s"$name $description")
 
   override def toString = s"$id $startsAt ${name()(defaultLang)} $minutes minutes, $clock, $nbPlayers players"
@@ -155,7 +158,7 @@ object Tournament {
       clock: ClockConfig,
       minutes: Int,
       variant: chess.variant.Variant,
-      position: StartingPosition,
+      position: Either[StartingPosition, FEN],
       mode: Mode,
       password: Option[String],
       waitMinutes: Int,
@@ -168,10 +171,11 @@ object Tournament {
   ) =
     Tournament(
       id = makeId,
-      name = name | {
-        if (position.initial) GreatPlayer.randomName
-        else position.shortName
-      },
+      name = name | (position match {
+        case Left(pos) if pos.initial => GreatPlayer.randomName
+        case Left(pos)                => pos.shortName
+        case _                        => GreatPlayer.randomName
+      }),
       status = Status.Created,
       clock = clock,
       minutes = minutes,
@@ -206,7 +210,7 @@ object Tournament {
       createdAt = DateTime.now,
       nbPlayers = 0,
       variant = sched.variant,
-      position = sched.position,
+      position = Left(sched.position),
       mode = Mode.Rated,
       conditions = sched.conditions,
       schedule = Some(sched),

WandererXII/lishogi

modules/tournament/src/main/BSONHandlers.scala
@@ -1,6 +1,7 @@
 package lila.tournament
 
 import shogi.Clock.{ Config => ClockConfig }
+import shogi.format.FEN
 import shogi.variant.Variant
 import shogi.{ Mode, StartingPosition }
 import lila.db.BSON
@@ -59,9 +60,11 @@ object BSONHandlers {
   implicit val tournamentHandler = new BSON[Tournament] {
     def reads(r: BSON.Reader) = {
       val variant = r.intO("variant").fold[Variant](Variant.default)(Variant.orDefault)
-      val position: StartingPosition = r.strO("fen").flatMap(Thematic.byFen) orElse
-        r.strO("eco").flatMap(Thematic.byEco) getOrElse // for BC
-        StartingPosition.initial
+      val position: Either[StartingPosition, FEN] =
+        r.strO("fen") map { f =>
+          Thematic.byFen(f).fold[Either[StartingPosition, FEN]](Right(FEN(f)))(Left(_))
+        } getOrElse
+          Left(r.strO("eco").flatMap(Thematic.byEco) | StartingPosition.initial)
       val startsAt   = r date "startsAt"
       val conditions = r.getO[Condition.All]("conditions") getOrElse Condition.All.empty
       Tournament(
@@ -82,7 +85,8 @@ object BSONHandlers {
           doc   <- r.getO[Bdoc]("schedule")
           freq  <- doc.getAsOpt[Schedule.Freq]("freq")
           speed <- doc.getAsOpt[Schedule.Speed]("speed")
-        } yield Schedule(freq, speed, variant, position, startsAt, conditions),
+          pos   <- position.left.toOption
+        } yield Schedule(freq, speed, variant, pos, startsAt, conditions),
         nbPlayers = r int "nbPlayers",
         createdAt = r date "createdAt",
         createdBy = r strO "createdBy" getOrElse lishogiId,
@@ -96,20 +100,24 @@ object BSONHandlers {
     }
     def writes(w: BSON.Writer, o: Tournament) =
       $doc(
-        "_id"        -> o.id,
-        "name"       -> o.name,
-        "status"     -> o.status,
-        "clock"      -> o.clock,
-        "minutes"    -> o.minutes,
-        "variant"    -> o.variant.some.filterNot(_.standard).map(_.id),
-        "fen"        -> o.position.some.filterNot(_.initial).map(_.fen),
-        "mode"       -> o.mode.some.filterNot(_.rated).map(_.id),
-        "password"   -> o.password,
-        "conditions" -> o.conditions.ifNonEmpty,
-        "teamBattle" -> o.teamBattle,
-        "noBerserk"  -> w.boolO(o.noBerserk),
-        "noStreak"   -> w.boolO(o.noStreak),
-        "schedule" -> o.schedule.map { s =>
+        "_id"     -> o.id,
+        "name"    -> o.name,
+        "status"  -> o.status,
+        "clock"   -> o.clock,
+        "minutes" -> o.minutes,
+        "variant" -> o.variant.some.filterNot(_.standard).map(_.id),
+        "fen" -> (o.position match {
+          case Right(f)              => f.value.some
+          case Left(p) if !p.initial => p.fen.some
+          case _                     => none
+        }),
+        "mode"        -> o.mode.some.filterNot(_.rated).map(_.id),
+        "password"    -> o.password,
+        "conditions"  -> o.conditions.ifNonEmpty,
+        "teamBattle"  -> o.teamBattle,
+        "noBerserk"   -> w.boolO(o.noBerserk),
+        "noStreak"    -> w.boolO(o.noStreak),
+        "schedule"    -> o.schedule.map { s =>
           $doc(
             "freq"  -> s.freq,
             "speed" -> s.speed

modules/tournament/src/main/JsonView.scala
@@ -1,5 +1,7 @@
 package lila.tournament
 
+import shogi.format.FEN
+import shogi.StartingPosition
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 import play.api.i18n.Lang
@@ -11,7 +13,6 @@ import lila.common.{ Animal, LightUser, Uptime }
 import lila.game.{ Game, LightPov }
 import lila.hub.LightTeam.TeamID
 import lila.memo.CacheApi._
-import lila.quote.Quote.quoteWriter
 import lila.rating.PerfType
 import lila.socket.Socket.SocketVersion
 import lila.user.{ LightUserApi, User }
@@ -116,7 +117,7 @@ final class JsonView(
           )
           .add("spotlight" -> tour.spotlight)
           .add("berserkable" -> tour.berserkable)
-          .add("position" -> full.option(tour.position).filterNot(_.initial).map(positionJson))
+          .add("position" -> full.??(positionJson(tour.position)))
           .add("verdicts" -> verdicts.map(Condition.JSONHandlers.verdictsFor(_, lang)))
           .add("schedule" -> tour.schedule.map(scheduleJson))
           .add("private" -> tour.isPrivate)
@@ -509,13 +510,25 @@ object JsonView {
     )
   }
 
-  private[tournament] def positionJson(s: shogi.StartingPosition) =
-    Json.obj(
-      "eco"      -> s.eco,
-      "name"     -> s.name,
-      "wikiPath" -> s.wikiPath,
-      "fen"      -> s.fen
-    )
+  private[tournament] def positionJson(s: Either[StartingPosition, FEN]): Option[JsObject] = s match {
+    case Left(pos) if pos.initial => none
+    case Left(pos) =>
+      Json
+        .obj(
+          "eco"      -> pos.eco,
+          "name"     -> pos.name,
+          "wikiPath" -> pos.wikiPath,
+          "fen"      -> pos.fen
+        )
+        .some
+    case Right(fen) =>
+      Json
+        .obj(
+          "name" -> "Custom position",
+          "fen"  -> fen.value
+        )
+        .some
+  }
 
   implicit private[tournament] val spotlightWrites: OWrites[Spotlight] = OWrites { s =>
     Json

modules/tournament/src/main/Tournament.scala
@@ -1,5 +1,6 @@
 package lila.tournament
 
+import shogi.format.FEN
 import org.joda.time.{ DateTime, Duration, Interval }
 import lila.common.ThreadLocalRandom
 import play.api.i18n.Lang
@@ -20,7 +21,7 @@ case class Tournament(
     clock: ClockConfig,
     minutes: Int,
     variant: shogi.variant.Variant,
-    position: StartingPosition,
+    position: Either[StartingPosition, FEN],
     mode: Mode,
     password: Option[String] = None,
     conditions: Condition.All,
@@ -138,6 +139,8 @@ case class Tournament(
 
   def ratingVariant = if (variant.fromPosition) shogi.variant.Standard else variant
 
+  def initialPosition = position.left.exists(_.initial)
+
   lazy val looksLikePrize = !isScheduled && lila.common.String.looksLikePrize(s"$name $description")
 
   override def toString = s"$id $startsAt ${name()(defaultLang)} $minutes minutes, $clock, $nbPlayers players"
@@ -157,7 +160,7 @@ object Tournament {
       clock: ClockConfig,
       minutes: Int,
       variant: shogi.variant.Variant,
-      position: StartingPosition,
+      position: Either[StartingPosition, FEN],
       mode: Mode,
       password: Option[String],
       waitMinutes: Int,
@@ -170,10 +173,11 @@ object Tournament {
   ) =
     Tournament(
       id = makeId,
-      name = name | {
-        if (position.initial) Animal.randomName
-        else position.shortName
-      },
+      name = name | (position match {
+        case Left(pos) if pos.initial => Animal.randomName
+        case Left(pos)                => pos.shortName
+        case _                        => Animal.randomName
+      }),
       status = Status.Created,
       clock = clock,
       minutes = minutes,
@@ -208,7 +212,7 @@ object Tournament {
       createdAt = DateTime.now,
       nbPlayers = 0,
       variant = sched.variant,
-      position = sched.position,
+      position = Left(sched.position),
       mode = Mode.Rated,
       conditions = sched.conditions,
       schedule = Some(sched),