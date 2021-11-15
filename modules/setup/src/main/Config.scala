lichess-org/lila

arena tournaments from arbitrary positions

modules/setup/src/main/Config.scala
@@ -62,8 +62,8 @@ trait Positional { self: Config =>
   def strictFen: Boolean
 
   lazy val validFen = variant != FromPosition || {
-    fen ?? { f =>
-      ~(Forsyth <<< f.value).map(_.situation playable strictFen)
+    fen exists { f =>
+      (Forsyth <<< f.value).exists(_.situation playable strictFen)
     }
   }
 

WandererXII/lishogi

modules/setup/src/main/Config.scala
@@ -68,8 +68,8 @@ trait Positional { self: Config =>
   def strictFen: Boolean
 
   lazy val validFen = variant != FromPosition || {
-    fen ?? { f =>
-      ~(Forsyth <<< f.value).map(_.situation playableNoImpasse strictFen)
+    fen exists { f =>
+      (Forsyth <<< f.value).exists(_.situation playableNoImpasse strictFen)
     }
   }
 