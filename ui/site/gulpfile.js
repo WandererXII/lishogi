947f5afcee04fd548e83f634d0ab107d453372c1
SriMethan <methanroyal@gmail.com>
2022-03-13 09:52:12 +0530

    Update Source url

--- ui/site/gulpfile.js
+++ ui/site/gulpfile.js
@@ -122,7 +122,7 @@ const gitSha = cb => {
   if (!fs.existsSync('./dist')) fs.mkdirSync('./dist');
   fs.writeFileSync(
     './dist/consolemsg.js',
-    `window.lishogi=window.lishogi||{};console.info("Lishogi is open source! https://github.com/WandererXII/lila");lishogi.info=${info};`
+    `window.lishogi=window.lishogi||{};console.info("Lishogi is open source! https://github.com/lishogi.org/source");lishogi.info=${info};`
   );
   cb();
 };
