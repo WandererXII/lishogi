lichess-org/lila

store new scores separately

app/controllers/Coordinate.scala
@@ -20,7 +20,8 @@ final class Coordinate(env: Env) extends LilaController(env) {
         .bindFromRequest()
         .fold(
           _ => fuccess(BadRequest),
-          data => env.coordinate.api.addScore(me.id, data.isWhite, data.score) inject Ok(())
+          data =>
+            env.coordinate.api.addScore(me.id, data.isFindSquareMode, data.isWhite, data.score) inject Ok(())
         )
     }
 

modules/coordinate/src/main/CoordinateApi.scala
@@ -13,18 +13,26 @@ final class CoordinateApi(scoreColl: Coll)(implicit ec: scala.concurrent.Executi
   def getScore(userId: User.ID): Fu[Score] =
     scoreColl.byId[Score](userId) map (_ | Score(userId))
 
-  def addScore(userId: User.ID, white: Boolean, hits: Int): Funit =
+  def addScore(userId: User.ID, findSquareMode: Boolean, white: Boolean, hits: Int): Funit =
     scoreColl.update
       .one(
         $id(userId),
         $push(
           $doc(
             "white" -> BSONDocument(
-              "$each"  -> (white ?? List(BSONInteger(hits))),
+              "$each"  -> ((findSquareMode && white) ?? List(BSONInteger(hits))),
               "$slice" -> -20
             ),
             "black" -> BSONDocument(
-              "$each"  -> (!white ?? List(BSONInteger(hits))),
+              "$each"  -> ((findSquareMode && !white) ?? List(BSONInteger(hits))),
+              "$slice" -> -20
+            ),
+            "whiteNameSquare" -> BSONDocument(
+              "$each"  -> ((!findSquareMode && white) ?? List(BSONInteger(hits))),
+              "$slice" -> -20
+            ),
+            "blackNameSquare" -> BSONDocument(
+              "$each"  -> ((!findSquareMode && !white) ?? List(BSONInteger(hits))),
               "$slice" -> -20
             )
           )

modules/coordinate/src/main/Score.scala
@@ -3,5 +3,7 @@ package lila.coordinate
 case class Score(
     _id: String,
     white: List[Int] = Nil,
-    black: List[Int] = Nil
+    black: List[Int] = Nil,
+    whiteNameSquare: List[Int] = Nil,
+    blackNameSquare: List[Int] = Nil
 )

WandererXII/lishogi

app/controllers/Coordinate.scala
@@ -20,7 +20,8 @@ final class Coordinate(env: Env) extends LilaController(env) {
         .bindFromRequest()
         .fold(
           _ => fuccess(BadRequest),
-          data => env.coordinate.api.addScore(me.id, data.isSente, data.score) inject Ok(())
+          data =>
+            env.coordinate.api.addScore(me.id, data.isFindSquareMode, data.isSente, data.score) inject Ok(())
         )
     }
 

modules/coordinate/src/main/CoordinateApi.scala
@@ -13,18 +13,26 @@ final class CoordinateApi(scoreColl: Coll)(implicit ec: scala.concurrent.Executi
   def getScore(userId: User.ID): Fu[Score] =
     scoreColl.byId[Score](userId) map (_ | Score(userId))
 
-  def addScore(userId: User.ID, sente: Boolean, hits: Int): Funit =
+  def addScore(userId: User.ID, findSquareMode: Boolean, sente: Boolean, hits: Int): Funit =
     scoreColl.update
       .one(
         $id(userId),
         $push(
           $doc(
             "sente" -> BSONDocument(
-              "$each"  -> (sente ?? List(BSONInteger(hits))),
+              "$each"  -> ((findSquareMode && sente) ?? List(BSONInteger(hits))),
               "$slice" -> -20
             ),
             "gote" -> BSONDocument(
-              "$each"  -> (!sente ?? List(BSONInteger(hits))),
+              "$each"  -> ((findSquareMode && !sente) ?? List(BSONInteger(hits))),
+              "$slice" -> -20
+            ),
+            "senteNameSquare" -> BSONDocument(
+              "$each"  -> ((!findSquareMode && sente) ?? List(BSONInteger(hits))),
+              "$slice" -> -20
+            ),
+            "goteNameSquare" -> BSONDocument(
+              "$each"  -> ((!findSquareMode && !sente) ?? List(BSONInteger(hits))),
               "$slice" -> -20
             )
           )

modules/coordinate/src/main/Score.scala
@@ -3,5 +3,7 @@ package lila.coordinate
 case class Score(
     _id: String,
     sente: List[Int] = Nil,
-    gote: List[Int] = Nil
+    gote: List[Int] = Nil,
+    senteNameSquare: List[Int] = Nil,
+    goteNameSquare: List[Int] = Nil
 )