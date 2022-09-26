lichess-org/lila

pass frame lag from lila-ws to scalachess for comp capping

modules/round/src/main/RoundSocket.scala
@@ -340,9 +340,14 @@ object RoundSocket {
               } yield PlayerDo(FullId(fullId), tpe)
             }
           case "r/move" =>
-            raw.get(5) { case Array(fullId, uciS, blurS, lagS, mtS) =>
+            raw.get(6) { case Array(fullId, uciS, blurS, lagS, mtS, fraS) =>
               Uci(uciS) map { uci =>
-                PlayerMove(FullId(fullId), uci, P.In.boolean(blurS), MoveMetrics(centis(lagS), centis(mtS)))
+                PlayerMove(
+                  FullId(fullId),
+                  uci,
+                  P.In.boolean(blurS),
+                  MoveMetrics(centis(lagS), centis(mtS), centis(fraS))
+                )
               }
             }
           case "chat/say" =>

WandererXII/lishogi

modules/round/src/main/RoundSocket.scala
@@ -266,9 +266,14 @@ object RoundSocket {
               } yield PlayerDo(FullId(fullId), tpe)
             }
           case "r/move" =>
-            raw.get(5) { case Array(fullId, usiS, blurS, lagS, mtS) =>
+            raw.get(6) { case Array(fullId, usiS, blurS, lagS, mtS, fraS) =>
               Usi(usiS) map { usi =>
-                PlayerMove(FullId(fullId), usi, P.In.boolean(blurS), MoveMetrics(centis(lagS), centis(mtS)))
+                PlayerMove(
+                  FullId(fullId),
+                  usi,
+                  P.In.boolean(blurS),
+                  MoveMetrics(centis(lagS), centis(mtS), centis(fraS))
+                )
               }
             }
           case "chat/say" =>