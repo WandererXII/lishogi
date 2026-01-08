62f81082f601476a2f3fc1cdca4c5ccddad3da69
WandererXII <43043181+WandererXII@users.noreply.github.com>
2026-01-08 23:29:42 +0100

    Only 9 seeks so register button is visible

--- modules/lobby/src/main/Env.scala
+++ modules/lobby/src/main/Env.scala
@@ -31,7 +31,7 @@ final class Env(
   private lazy val seekApiConfig = new SeekApi.Config(
     coll = db(CollName("seek")),
     archiveColl = db(CollName("seek_archive")),
-    maxPerPage = MaxPerPage(10),
+    maxPerPage = MaxPerPage(9),
     maxPerUser = Max(5),
     maxHard = Max(255),
   )
