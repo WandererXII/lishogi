b90c4129204bfd9e7ecb14a9b0c7eacc3ba01f9a
WandererXII <43043181+WandererXII@users.noreply.github.com>
2026-02-24 23:23:21 +0100

    Dobutsu checkmate is the end for fairy

--- ui/ceval/src/util.ts
+++ ui/ceval/src/util.ts
@@ -13,8 +13,24 @@ export function renderEval(e: number): string {
 export const unsupportedVariants: VariantKey[] = ['chushogi', 'annanshogi'];
 
 export function invalidPosition(pos: Position): boolean {
-  // Opposite check fails
   if (pos.rules === 'dobutsu') {
+    // Fail - if we are in check and all moves lead to check
+    if (pos.isCheck()) {
+      const allMoves = pos.allMoveDests();
+      if (
+        [...allMoves.entries()].every(([orig, dests]) => {
+          return [...dests].every(dest => {
+            const clone = pos.clone();
+            clone.play({ from: orig, to: dest });
+            clone.turn = opposite(clone.turn);
+            return clone.isCheck();
+          });
+        })
+      )
+        return true;
+    }
+
+    // Fail - if opposite check
     const oppositePos = pos.clone();
     oppositePos.turn = opposite(pos.turn);
 
